8d834fab70079ecc13a80b7839e2573f662d6958
Gokul Kannan <gokulskannan1993@gmail.com>
2024-06-10 08:31:52 +0100

    almost done, only edit remains

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,34 @@
 import React from "react";
 
-export const todo = () => {
-  return <div>todo</div>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+  return (
+    <div className="Todo">
+      <p
+        className={`${task.completed ? "completed" : ""}`}
+        onClick={() => toggleComplete(task.id)}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          onClick={() => {
+            editTodo(task.id);
+          }}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          onClick={() => {
+            deleteTodo(task.id);
+          }}
+        />{" "}
+      </div>
+    </div>
+  );
 };
--- src/components/TodoEdit.js
+++ src/components/TodoEdit.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoEdit = () => {
-  return <div>TodoEdit</div>;
+export const TodoEdit = (editTodo, task) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    // prevent default action
+    e.preventDefault();
+    if (value) {
+      // add todo
+      editTodo(value, task.id);
+      // clear form after submission
+      setValue("");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="Update Task"
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
 };
